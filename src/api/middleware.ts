520828ea27a56ed64663d7be89dc3d1de73a90e0
Abdelrahman Altamimi <tamimi6030@gmail.com>
2025-11-18 21:34:29 +0300

    add error handling middleware

---
+++ src/api/middleware.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { config } from "../config.js";
+import { respondWithError } from "./json.js";
+
+
+export function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        if (res.statusCode >= 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`)
+        }
+    });
+    next()
+}
+
+export function middlewareMetricsInc(_: Request, __: Response, next: NextFunction) {
+    config.fileServerHits++;
+    next()
+}
+
+export function errorMiddleware(
+    err: Error,
+    _: Request,
+    res: Response,
+    __: NextFunction,
+) {
+    let statusCode = 500;
+    let message = "Something went wrong on our end";
+
+    console.log(err.message);
+
+    respondWithError(res, statusCode, message);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
-import { middlewareLogResponses } from "./api/middelware.js";
+import { errorMiddleware, middlewareLogResponses, middlewareMetricsInc } from "./api/middleware.js";
+import { handlerMetrics } from "./api/metrics.js";
+import { handlerReset } from "./api/reset.js";
+import { handlerChirpsValidate } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/healthz",  handlerReadiness);
+app.get("/api/healthz", (req, res, next) => {
+    Promise.resolve(handlerReadiness(req, res)).catch(next);
+});
+app.get("/admin/metrics", (req, res, next) => {
+    Promise.resolve(handlerMetrics(req, res)).catch(next);
+});
+app.post("/admin/reset", (req, res, next) => {
+    Promise.resolve(handlerReset(req, res)).catch(next);
+});
+
+app.post("/api/validate_chirp", (req, res, next) => {
+    Promise.resolve(handlerChirpsValidate(req, res)).catch(next);
+});
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
